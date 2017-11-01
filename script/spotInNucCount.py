442991af0c37d8d20ecf4a42f18c3ab2e1c0f20c
benoitlo <benoit.lombardot@gmail.com>
2017-11-01 17:23:29 +0100

    add 2 example scripts and associated sample data

---
+++ script/spotInNucCount.py
@@ -0,0 +1,26 @@
+#@Img img0
+
+#@CIP cip
+#@UIService ui
+
+# the script can be used to count the spots in nuclei_transcripts.tif
+
+
+img = cip.duplicate(img0)
+
+# segment the nucleus
+imgNuc = cip.opening( img, 5 )
+imgNuc = cip.threshold(imgNuc, 'otsu')
+
+# detect spots
+imgSpot = cip.tophat( img, 5 )
+imgSpot = cip.gauss( img, 1 )
+imgSpot = cip.maxima( imgSpot, 'h', 200 )
+
+# spot in nucleus
+imgNucSpot = cip.mul(imgNuc, imgSpot)
+imgNucSpot = cip.label(imgNucSpot, 0.5 )
+
+
+ui.show(imgNuc)
+ui.show(imgNucSpot)
