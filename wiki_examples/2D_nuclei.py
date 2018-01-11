469a65b90778f5b13a5ef035c36904b3781e1180
benoitlo <benoit.lombardot@gmail.com>
2018-01-11 17:28:47 +0100

    add script examples for the wiki documentation

---
+++ wiki_examples/2D_nuclei.py
@@ -0,0 +1,31 @@
+#@ImagePlus img
+#@CIP cip
+
+# CIP example: a simple 2d cell measure. can be run with the image 
+# 	2D_nuclei.tif available on the CIP repository
+# Author: Benoit Lombardot
+
+# crop the image
+origin = cip.list(300,200)
+size = cip.list(150,150)
+img_dup = cip.duplicate(img , origin, size)
+
+# filter and segment the nuclei
+img_filt = cip.opening( img_dup , 5 )
+img_seg  = cip.watershed( img_filt , 'threshold', 500 , 'hmin', 300)
+
+# measure nuclei size
+regions = cip.region(img_seg, 'nuclei')
+measures  = cip.measure( regions , 'size')
+
+# visualize intermediary steps
+cip.show(img_dup)
+cip.show(img_filt)
+cip.show(img_seg, 'glasbey')
+
+# visualize results 
+h = cip.show(img_dup)
+cip.show( h , regions, 'glasbey' )
+cip.show(measures)
+
+
---
+++ wiki_examples/3D_nuclei_segmentation.py
@@ -0,0 +1,34 @@
+#@ImagePlus img
+#@Float blurRadius
+#@CIP cip
+
+# CIP example: a simple 3d cell measure. can be run with the image 
+# 	3D_nuc_mem_full_DS.tif available on the CIP repository
+# Author: Benoit Lombardot
+
+# duplicate the nuclei channel
+img_nuc = cip.slice( img , 'dimensions',2 , 'position',0 )
+
+# blurring using the image pixel size
+spacing = cip.spacing(img_nuc)
+radii = [blurRadius/r for r in spacing]
+img_blur = cip.gauss(img_nuc, cip.list(radii) )
+
+# segmentation of the nuclei
+threshold = cip.threshold(img_blur, 'moments', 'value')
+img_seg = cip.label(img_blur, threshold )
+
+# measure of nuclei size in image unit
+regions = cip.region(img_seg, 'nuclei3D_')
+measures  = cip.measure( regions , 'size')
+
+# visualize intermediary step and measures
+cip.show( img_nuc )
+cip.show( img_blur )
+cip.show( img_seg, 'glasbey_inverted' )
+cip.show( measures )
+
+
+
+
+
